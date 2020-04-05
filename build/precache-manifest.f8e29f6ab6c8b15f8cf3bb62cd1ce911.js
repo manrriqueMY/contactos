7437d3ed54f42c6bdad764a324ae0e0d97540de5
manrriqueMY <manrrique.my@gmail.com>
2020-04-05 10:28:25 -0500

    yh

---
+++ build/precache-manifest.f8e29f6ab6c8b15f8cf3bb62cd1ce911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd97260581f4b03bd5fa9b98be4f6be1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d3e9c7b5e6ef6b487d1",
+    "url": "/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "94aee13a5521de7e315a",
+    "url": "/static/js/2.117fc339.chunk.js"
+  },
+  {
+    "revision": "b73db60337a63448fc0f0ed577e37ab5",
+    "url": "/static/js/2.117fc339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3e9c7b5e6ef6b487d1",
+    "url": "/static/js/main.b74e6daa.chunk.js"
+  },
+  {
+    "revision": "f19055977cf5101d9625",
+    "url": "/static/js/runtime-main.c6d9462e.js"
+  }
+]);
