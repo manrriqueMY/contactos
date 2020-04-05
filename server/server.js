45ea5431016437b6fd7f3df18b58f89f2ce7f421
manrriqueMY <manrrique.my@gmail.com>
2020-04-05 10:20:05 -0500

    tg

--- server/server.js
+++ server/server.js
@@ -1,18 +1,33 @@
-var express=require("express");
-var ruta=express.Router();
-var app=express();
+const express=require("express");
+const ruta=express.Router();
+const app=express();
+const cors = require('cors');
+const path = require('path');
+const mongo=require("mongoose");
+const port = process.env.PORT || 3000;
 
-var mongo=require("mongoose");
-mongo.connect("mongodb://contacto:abc123@ds161764.mlab.com:61764/contactos").then(db=>console.log('DB esta Conectado')).catch(error=>console.log(error));
+mongo.connect("mongodb://contacto:abc123@ds161764.mlab.com:61764/contactos",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+}).then(db=>console.log('DB esta Conectado')).catch(error=>console.log(error));
+//mongo.connect("mongodb://localhost:27017/contactos").then(db=>console.log('DB esta Conectado')).catch(error=>console.log(error));
 
+/*
 app.get("/",function(req,res){
 	res.send("Conectado");
-});
+});*/
 
 var contact=require("./dao/contacto");
 app.use(express.json());
+app.use(cors());
 app.use("/api/contacto",contact);
 
-app.listen(3001,function(){
-	console.log("Corriendo en 3001");
+app.use(express.static(path.join(__dirname, '/../build')));
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/../build/index.html'));
+});
+
+app.listen(port,function(){
+	console.log("Corriendo en "+port);
 });
